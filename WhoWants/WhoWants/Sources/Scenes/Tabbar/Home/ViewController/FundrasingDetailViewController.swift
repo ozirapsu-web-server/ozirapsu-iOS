28b7d08bbca4416172b73582b3e6eefe79c1bee4
dongminyoon <ehdals8965@naver.com>
2020-12-08 15:29:50 +0900

    [UPDATE] 모금하기 상세 페이지 ViewController 생성

---
+++ WhoWants/WhoWants/Sources/Scenes/Tabbar/Home/ViewController/FundrasingDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FundrasingDetailViewController.swift
+//  WhoWants
+//
+//  Created by 윤동민 on 2020/12/08.
+//
+
+import UIKit
+
+class FundrasingDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
