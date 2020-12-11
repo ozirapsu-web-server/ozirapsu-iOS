c9b66fdaceaa5378ab8e6e163b8e0cf06e0b26c3
dongminyoon <ehdals8965@naver.com>
2020-12-12 02:10:45 +0900

    [UPDATE] 탭바 아이콘 변경

---
+++ WhoWants/WhoWants/Sources/APIServices/Signup/SignupService.swift
@@ -0,0 +1,17 @@
+//
+//  SignupService.swift
+//  WhoWants
+//
+//  Created by 윤동민 on 2020/12/12.
+//
+
+import Foundation
+import Alamofire
+
+struct SignupService {
+    static let shared = SignupService()
+    
+    private init() { }
+    
+    
+}
