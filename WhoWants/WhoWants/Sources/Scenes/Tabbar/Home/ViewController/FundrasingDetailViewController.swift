09d99626052b7e971b342ba046a78af3516d86ff
dongminyoon <ehdals8965@naver.com>
2020-12-12 05:53:49 +0900

    [UPDATE] 기부하기 상세 뷰 - 이미지 페이징 완성

--- WhoWants/WhoWants/Sources/Scenes/Tabbar/Home/ViewController/FundrasingDetailViewController.swift
+++ WhoWants/WhoWants/Sources/Scenes/Tabbar/Home/ViewController/FundrasingDetailViewController.swift
@@ -16,9 +16,12 @@ class FundrasingDetailViewController: UIViewController {
     let titleLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
-        label.text = FundrasingDetailText.title.rawValue
-        label.font = UIFont.systemFont(ofSize: 18)
-        label.textColor = .mainblack
+        let attributeText = NSAttributedString(string: FundrasingDetailText.title.rawValue,
+                                               attributes: [.font: UIFont(name: FontName.notosans_medium,
+                                                                          size: 18)!,
+                                                            .kern: -0.72,
+                                                            .foregroundColor: UIColor.mainblack])
+        label.attributedText = attributeText
         return label
     }()
     
@@ -38,8 +41,35 @@ class FundrasingDetailViewController: UIViewController {
         return stackView
     }()
     
+    let topContainerView: UIView = {
+        let uiView = UIView()
+        uiView.backgroundColor = .clear
+        return uiView
+    }()
+    
+    lazy var imageCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width,
+                                 height: UIScreen.main.bounds.width*0.66)
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = .zero
+        layout.minimumLineSpacing = 0
+        
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: layout)
+        collectionView.register(FundraisingImageCell.self,
+                                forCellWithReuseIdentifier: FundraisingImageCell.identifier)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isPagingEnabled = true
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     // MARK: - Init
     private func setNav() {
+        self.navigationController?.navigationBar.transform = .identity
         self.navigationController?.navigationBar.tintColor = .mainblack
         self.navigationItem.setHidesBackButton(true, animated: true)
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: ImageName.backBtn),
@@ -52,6 +82,13 @@ class FundrasingDetailViewController: UIViewController {
                                                                  target: self,
                                                                  action: #selector(write(_:)))
         self.navigationItem.titleView = titleLabel
+    }
+    
+    private func initView() {
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(contentStackView)
+        contentStackView.addArrangedSubview(topContainerView)
+        topContainerView.addSubview(imageCollectionView)
     }
     
     // MARK: - Action
@@ -72,9 +109,49 @@ class FundrasingDetailViewController: UIViewController {
         self.view.backgroundColor = .white
         
         setNav()
+        initView()
+        configureLayout()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
+    
+    // MARK: - Layout
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            contentStackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentStackView.leadingAnchor.constraint(equalTo: contentStackView.leadingAnchor),
+            contentStackView.trailingAnchor.constraint(equalTo: contentStackView.trailingAnchor),
+            contentStackView.bottomAnchor.constraint(equalTo: contentStackView.bottomAnchor),
+            contentStackView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
+            imageCollectionView.topAnchor.constraint(equalTo: topContainerView.topAnchor),
+            imageCollectionView.leadingAnchor.constraint(equalTo: topContainerView.leadingAnchor),
+            imageCollectionView.trailingAnchor.constraint(equalTo: topContainerView.trailingAnchor),
+            imageCollectionView.bottomAnchor.constraint(equalTo: topContainerView.bottomAnchor),
+            imageCollectionView.heightAnchor.constraint(equalTo: topContainerView.widthAnchor, multiplier: 0.66)
+        ])
+    }
+}
+
+extension FundrasingDetailViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let fundraisingImageCell = collectionView.dequeueReusableCell(withReuseIdentifier: FundraisingImageCell.identifier,
+                                                                            for: indexPath)
+                as? FundraisingImageCell else { return UICollectionViewCell() }
+        fundraisingImageCell.bind()
+        return fundraisingImageCell
+    }
+}
+
+extension FundrasingDetailViewController: UICollectionViewDelegate {
+    
 }
